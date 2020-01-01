3dce9baa5829556a0a1a8a3fe7d9f44c59e89bd0
Shawnqqq <1206190166@qq.com>
2020-01-01 15:51:28 +0800

    demo config

---
+++ config.demo.js
@@ -0,0 +1,19 @@
+const configs = {
+  mysql: {
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: '123456',
+    database: 'topics-ycl'
+  },
+  wechat:{
+    appid: '',
+    secret: ''
+  },
+  qiniu: {
+    accessKey: '',
+    secretKey: ''
+  }
+}
+
+module.exports = configs
